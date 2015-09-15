776a2a54905ecdab1fa8a1bc087c00c136f4706d
Justus <justus.schleicher@gmail.com>
2015-09-15 16:08:55 +0200

    Grundgerüst für die Entwicklung

--- server/server.js
+++ server/server.js
@@ -2,27 +2,28 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var dbConf = require('./db');
-var logger = require('morgan');
+var path = require('path');
+
 
 var app = express();
 
-app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
 
-app.all('/*', function(req, res, next) {
-  // CORS headers
-  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  // Set custom headers for CORS
-  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
-  //res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-  if (req.method == 'OPTIONS') {
-    res.status(200).end();
-  } else {
-    next();
-  }
+app.all('/*', function (req, res, next) {
+    // CORS headers
+    res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    // Set custom headers for CORS
+    res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+    //res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+    if (req.method == 'OPTIONS') {
+        res.status(200).end();
+    } else {
+        next();
+    }
 });
+
 
 // connect to mongolab
 mongoose.connect(dbConf.url);
@@ -31,23 +32,27 @@ mongoose.connect(dbConf.url);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
-  logger.info('connected to mongolab');
+    console.log('connected to mongolab' + dbConf.url);
 });
 
 app.use('/', require('./routes'));
 
 app.use(express.static(__dirname + '/../app'));
 
+app.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname + '/../app/index.html'));
+});
+
 // If no route is matched by now, it must be a 404
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // Start the server
 app.set('port', process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
+var server = app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + server.address().port);
 });
