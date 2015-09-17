257f97edba72eadca5857db25c2a33d697eb5595
Justus <justus.schleicher@gmail.com>
2015-09-17 22:37:21 +0200

    view#7 settings

---
+++ app/www/views/settings/settingsController.js
@@ -0,0 +1,16 @@
+angular.module('app')
+
+    .controller('settingsController', function ($scope, $location) {
+        
+        $scope.deleteAccount = function () {
+                
+        };
+        
+        $scope.gpsData = function () {
+            //check status of checkbox
+            //if checked 
+        };
+        
+        $scope.pushMessages = function () {
+        }
+    });
